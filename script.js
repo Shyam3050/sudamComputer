2f92bab506ccf58ae2d11365e1aaabf62551aad4
shyam3050 <nimmanashyam3050@gmail.com>
2022-06-15 16:39:13 +0530

    new repo

---
+++ script.js
@@ -0,0 +1,29 @@
+// for querySelector
+const getElement = (selector) => {
+  const element = document.querySelector(selector);
+  if (element) return element;
+  throw Error(`please check the class names, there is no ${selector}`);
+};
+// for querySelectorAll
+const getAllElement = (selector) => {
+  const element = document.querySelectorAll(selector);
+  if (element) return element;
+  throw Error(
+    `All-Selector Say! please check the class names, there is no ${selector}`
+  );
+};
+const toggle_btn = getElement(".toggle-btn");
+const nav_links = getElement(".nav-links");
+const icon = getElement(".icon");
+let toogleClick = 0;
+toggle_btn.addEventListener("click", () => {
+  nav_links.classList.toggle("show-link");
+  if (toogleClick == 0) {
+    icon.classList = "fa fa-times icon";
+    return (toogleClick = 1);
+  }
+  if (toogleClick == 1) {
+    icon.classList = "fa fa-bars icon";
+    return (toogleClick = 0);
+  }
+});
